2cad663a8b7f3142a979bff24cbfe385dbebb314
‘Sawsanhenedy’ <sawsanehendy92@gmail.com>
2023-05-02 05:13:54 -0700

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+ * puts2 - prints all string's characters
+ * @str: string parameter input
+ * Return: Nothing
+ */
+void puts2(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; ++i)
+{
+if (i % 2 == 0)
+_putchar(str[i]);
+}
+_putchar('\n');
+}
