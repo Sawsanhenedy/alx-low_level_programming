2c98d86df2872c4c9ed56ceed8dde7e3972d38b2
‘Sawsanhenedy’ <sawsanehendy92@gmail.com>
2023-05-04 12:31:51 -0700

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,8 +7,8 @@
 char *leet(char *c)
 {
 char *cp = c;
-char key[] = ('A', 'E', 'O', 'T', 'L');
-int value[] = (4, 3, 0, 7, 1);
+char key[] = {'A', 'E', 'O', 'T', 'L'};
+int value[] = {4, 3, 0, 7, 1};
 unsigned int i;
 while (*c)
 {
