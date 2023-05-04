25d2ee68b6ee805db623641c6355fd9eed0f7671
‘Sawsanhenedy’ <sawsanehendy92@gmail.com>
2023-05-04 13:09:59 -0700

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,9 +7,9 @@
 char *rot13(char *s)
 {
 int i;
-char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"
-char *prt = s;
+char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+char *ptr = s;
 while (*s)
 {
 for (i = 0; i <= 52; i++)
