dd4ac52ea85b106257ee812c123f33a16b9bf90c
‘Sawsanhenedy’ <sawsanehendy92@gmail.com>
2023-05-04 10:25:33 -0700

    C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ * Return: Nothing
+ */
+void reverse_array(int *a, int n)
+{
+int b, c, d;
+for (b = 0, c = (n - 1); b < c; b++; c--)
+{
+d = a[b];
+a[b] = a[c];
+a[c] = d;
+}
+}
