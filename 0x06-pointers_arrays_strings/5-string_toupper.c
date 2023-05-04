0a6b46ac117d9581edb4d9759bfeb6784956658e
‘Sawsanhenedy’ <sawsanehendy92@gmail.com>
2023-05-04 10:41:47 -0700

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,6 +1,7 @@
 #include"main.h"
 /**
- * *string_toupper - changes all lowercase letters of a string to uppercase
+ * *string_toupper - changes all lowercase letters of
+ * a string to uppercase
  * @str: string to be returned
  * Return: @str
  */
