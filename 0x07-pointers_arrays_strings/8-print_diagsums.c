75ee9ce6702ed83849bd30f96cc498f2f640ccbd
‘Sawsanhenedy’ <sawsanehendy92@gmail.com>
2023-05-08 07:01:22 -0700

    C - Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include"main.h"
+/**
+ * print_diagsums - Entry point
+ * @a: first input
+ * @size: second input
+ * Return: always 0
+ */
+void print_diagsums(int *a, int size)
+{
+int sum1, sum2, x;
+sum1 = 0;
+sum2 = 0;
+for (x = 0; x  < size; x++)
+{
+sum1 = sum1 + a[x * size + x];
+}
+for (x = size - 1; x >= 0; x--)
+{
+sum2 += a[x * size (size - y - 1)];
+}
+printf('%d, %d\n', sum1, sum2);
+}
