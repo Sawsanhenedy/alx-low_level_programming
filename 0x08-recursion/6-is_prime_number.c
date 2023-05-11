8db8352b9fc4cbc1eb2420a7da66cb0e5ed69c9b
‘Sawsanhenedy’ <sawsanehendy92@gmail.com>
2023-05-11 00:53:10 -0700

    C - Recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,28 @@
+#include"main.h"
+/**
+ * is_prime_number - returns 1 if the input integer is a prime number
+ * otherwise return 0
+ * @n: integer
+ * @othern: integer
+ * Return: 0 or 1
+ */
+int check_prime(int n, int othern);
+int is_prime_number(int n)
+{
+return (check_prime(n, 2));
+}
+/**
+ * check_prime - checks all numbers < n if they can be divided
+ * @n: integer
+ * @othern: integer
+ * Return: integer
+ */
+int check_prime(int n, int othern);
+{
+if (othern >= n && n > 1)
+return (1);
+else if (n % othern == 0 || n <= 1)
+return (0);
+else
+return (check_prime(n, othern + 1));
+}
