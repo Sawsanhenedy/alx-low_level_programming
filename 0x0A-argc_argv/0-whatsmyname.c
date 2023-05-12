c2904948de0d348a2f1c9d01e64e8b17398f03f3
‘Sawsanhenedy’ <sawsanehendy92@gmail.com>
2023-05-12 05:24:51 -0700

    C - argc, argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ * main - prints the name of the program argc _attribute_((unused))
+ * @argc: number of arguments
+ * @argv: Array of arguments
+ * Return: always 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", *argv);
+return (0);
+}
